d0525398d06d5efbfb2226184aa82e06e4066700
kHigasa <kojihigasa@gmail.com>
2018-12-08 00:18:45 +0900

    Add scaffold

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,17 @@
 import 'package:flutter/material.dart';
 
 main() {
-
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   build(context) {
-    return MaterialApp();
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('EazyList'),
+        ),
+      ),
+    );
   }
 }
